369f6be23bf3339da0f4d91fc7c0f1162fe13f2d
Damian Koper <kopernickk@gmail.com>
2019-11-24 00:58:43 +0100

    Chyba ok

--- src/main/java/populationRegistry/App.java
+++ src/main/java/populationRegistry/App.java
@@ -1,13 +1,25 @@
 package populationRegistry;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        Class app = App.class;
+        try {
+            App a = (App) app.getConstructors()[0].newInstance();
+            a.xd();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                | SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+    public void xd(){
         System.out.println( "Hello World!" );
+
     }
 }
